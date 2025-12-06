baa4147b92c711254e4546b6d785b886803a2bb9
Atharvakokate515 <katharva515@gmail.com>
2025-12-06 19:00:25 +0530

    Update streamlit_frontend_streaming.py

--- streamlit_frontend_streaming.py
+++ streamlit_frontend_streaming.py
@@ -12,7 +12,7 @@ if 'message_history' not in st.session_state:
 for message in st.session_state['message_history']:
     with st.chat_message(message['role']):
         st.text(message['content'])
-
+        
 #{'role': 'user', 'content': 'Hi'}
 #{'role': 'assistant', 'content': 'Hi=ello'}
 
